894ee30ad3529a78b7e4395f19aa72e1ef9b8bb9
PacoChan <maikelchan88@gmail.com>
2014-10-26 22:21:31 +0100

    - Algunos ajustes.

--- js/other.js
+++ js/other.js
@@ -10,3 +10,36 @@ $(window).load(function()
 		scrollInertia: 200
 	});
 });
+
+//Guías
+function loadGuide(url, id)
+{
+	$.ajax( url ).done(function(v)
+	{
+		$("#guide_container").css('position', 'relative');
+		$("#guide_container").animate({ opacity: 0, left: '20px'}, 200, function()
+		{
+			$("a[data-type='guide']").each(function(i, e)
+			{
+				$(e).toggleClass('selected', ($(e).data('id') == id));
+			});
+			$( "#guide_container").css('left', '-20px');
+			//$( ".mCSB_container").css('top', 0);
+			$( "#guide_container").html(v);
+			$( "#guide_container").animate({ opacity: 1, left: 0}, 200);
+		});
+		$(e).toggleClass('selected', ($(e).data('id') == id));
+	}).fail(function(e)
+	{
+		$( "#guide_container").html('error:' + e);
+	});
+}
+
+$("a[data-type='guide']").click(function(e)
+{
+	var element = $(e.target);
+	loadGuide(element.attr('href'), element.data('id'));
+	return false;
+});
+
+loadGuide("content/guides/secondary_missions.html", 'secondary_missions');
